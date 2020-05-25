feefdb59ad59e2d3d50a881fcbd0198c6aa0d631
opohorelov <oleksandr.pohorelov@jevera.software>
2020-05-25 21:24:12 +0300

    Initial commit

---
+++ src/test/java/com/pogorelov/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pogorelov.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
